6d87e2639d06a057006c43f5dddd3ce43c525fea
benkis <benkisenge03@gmail.com>
2023-09-06 23:31:04 +0200

    Create booking router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import './App.css'
 import Navbar from './layouts/navbar'
 import Header from './components/header'
 import { Service } from './components/Services'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Footer } from './layouts/footer'
+import Booking from './components/Booking'
 
 function App() {
   return (
     <div className="w-full">
       <Router>
         <Navbar />
-        <Header />
-        <Service />
+        <Routes>
+          <Route path="/booking" element={<Booking />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Service />
+              </>
+            }
+          />
+        </Routes>
+        <Footer />
       </Router>
     </div>
   )
