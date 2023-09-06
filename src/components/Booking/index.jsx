586bfc2ca141a752f517b470a5ea59b69f0d433a
benkis <benkisenge03@gmail.com>
2023-09-06 23:31:59 +0200

    Create Booking component

---
+++ src/components/Booking/index.jsx
@@ -0,0 +1,18 @@
+const Booking = () => {
+  return (
+    <div className={`home bg-cover bg-no-repeat w-full mt-20`}>
+      <div className="z-10 w-full h-full bg-black/75">
+        <div className="text-white flex flex-col justify-end  h-full px-20 pb-20 ml-20">
+          <form action="">
+
+          </form>
+          <div>
+            <h1>B</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Booking
