fb980ebe52863bd4c2133f4e99142939c98df351
benkis <benkisenge03@gmail.com>
2023-09-06 23:31:37 +0200

    Change booking directory

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
           <h1 className="uppercase font-medium text-5xl">ready, now.</h1>
           <p className="text-5xl font-light mt-2 mb-10">Anywhere, Anytime</p>
           <NavLink
-            to="/messages"
+            to="/booking"
             className={`px-8 py-2 bg-blue-500 font-bold rounded-xl w-[14%] hover:bg-blue-500/75`}
           >
             Book Now
