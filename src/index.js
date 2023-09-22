fdc50a30ceae853c79870f2cf092f56aee9d38db
joel-kalema <joelkalema63@gmail.com>
2023-09-23 01:31:16 +0200

    instal talwind material

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+import { ThemeProvider } from "@material-tailwind/react";
+
 import {
   ClerkProvider,
   SignedIn,
@@ -18,7 +21,9 @@ const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
 
