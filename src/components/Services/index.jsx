ecb9978eb157f9147ef6fd9c2f544cfb3352d48d
joel-kalema <joelkalema63@gmail.com>
2023-09-23 20:01:38 +0200

    updated style

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,12 +1,12 @@
 export function Service() {
   return (
     <div className="md:px-40 py-10" id="service">
-      <div className=" flex">
-        <div className="w-[50%]">
+      <div className="py-20 flex flex-col md:flex-row">
+        <div className="w-[80%] mx-auto md:w-[50%]">
           <img src="phone.png" alt="register" />
         </div>
-        <div className="w-[50%]">
-          <h1 className="font-medium px-4 py-2 mb-5">
+        <div className="w-[80%] shadow-xl mx-auto md:w-[50%] bg-blue-500 text-[#fff] rounded-b-[1rem] md:rounded-b-[4rem] px-10 py-20">
+          <h1 className="font-medium text-2xl md:text-3xl py-2 mb-5">
             What we offer
           </h1>
           <p className="font-light">
@@ -17,11 +17,11 @@ export function Service() {
           </p>
         </div>
       </div>
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="border font-medium px-4 py-2 border-black rounded-xl mb-5">
+      <div className="w-[80%] md:w-[60%] mx-auto md:py-20">
+        <h1 className="font-extrabold px-4 py-2 text-4xl md:text-6xl mb-5">
           Choose us
         </h1>
-        <p className="font-light text-center">
+        <p className="font-light">
           Choose us Whether you are a shipping company, a travel agency, or an
           individual looking to reserve a ship, our website streamlines the
           billing process, saving you time and effort. Experience the
