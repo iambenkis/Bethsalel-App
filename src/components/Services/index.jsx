3b875a6b0079557d8d575c9def8de47a395ac9ac
joel-kalema <joelkalema63@gmail.com>
2023-09-23 03:52:30 +0200

    updated styles

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,16 +1,21 @@
 export function Service() {
   return (
-    <div className="px-80 py-10" id="service">
-      <div className="flex flex-col justify-center items-center mb-10">
-        <h1 className="border font-medium px-4 py-2 border-black rounded-xl mb-5">
-          What we offer
-        </h1>
-        <p className="font-light text-center">
-          Our website offers a range of features designed to simplify your
-          billing processes. You can generate detailed bills and invoices with
-          just a few clicks, ensuring accurate and transparent financial
-          records.
-        </p>
+    <div className="md:px-40 py-10" id="service">
+      <div className=" flex">
+        <div className="w-[50%]">
+          <img src="phone.png" alt="register" />
+        </div>
+        <div className="w-[50%]">
+          <h1 className="font-medium px-4 py-2 mb-5">
+            What we offer
+          </h1>
+          <p className="font-light">
+            Our website offers a range of features designed to simplify your
+            billing processes. You can generate detailed bills and invoices with
+            just a few clicks, ensuring accurate and transparent financial
+            records.
+          </p>
+        </div>
       </div>
       <div className="flex flex-col justify-center items-center">
         <h1 className="border font-medium px-4 py-2 border-black rounded-xl mb-5">
