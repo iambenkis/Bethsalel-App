4a940c47124a6dbf43e63a2770b6839bb7c13543
benkis <benkisenge03@gmail.com>
2023-09-24 23:37:11 +0200

    Update the navbar layout

--- src/layouts/navbar.jsx
+++ src/layouts/navbar.jsx
@@ -1,10 +1,58 @@
 import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { CiUser } from 'react-icons/ci'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { FaCaretDown } from 'react-icons/fa'
+import axios from 'axios'
+import jwt_decode from 'jwt-decode'
+import { Logout } from '../redux/login'
+
+const axiosConfig = {
+  method: 'GET', // Specify the HTTP method (GET, POST, etc.)
+  url: 'http://localhost:3000/api/login', // Specify the API endpoint
+  mode: 'cors', // Set CORS mode to 'cors'
+}
 
 export default function Navbar() {
   const [isActive, setActive] = useState(false)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const state = useSelector((state) => state.login)
+  const [name, setName] = useState('')
+  const [activeMenu, setActiveMenu] = useState(false)
+  const token = localStorage.getItem('bet_token')
 
+  axios.defaults.withCredentials = true
+
+  useEffect(() => {
+    try {
+      // const token = localStorage.getItem('bet_token')
+      const name = jwt_decode(token).name
+      const firstNameInitial = name.split(' ')[0].charAt(0)
+      const lastNameInitial = name.split(' ')[1].charAt(0)
+      if (token === null) {
+        navigate('/login')
+      }
+      setName(name)
+    } catch (error) {
+      // navigate('/login')
+    }
+  }, [state.isAuthenticated])
+
+  const handleMenu = () => {
+    setActiveMenu(!activeMenu)
+  }
+
+  const handleLogout = () => {
+    dispatch(Logout())
+    console.log('logout')
+    setName('')
+    navigate('/login')
+  }
+
+  console.log(state, 'nav')
   return (
     <div
       className="fixed z-100 left-0 top-0 flex w-full bg-white justify-between items-center px-40 py-5 pl-40"
@@ -46,8 +94,33 @@ export default function Navbar() {
           </NavLink>
         </li>
       </ul>
-      <div>
-        <CiUser className="text-xl cursor-pointer" />
+      <div className="relative">
+        {name ? (
+          <div className="flex cursor-pointer" onClick={() => handleMenu()}>
+            <p className="text-sm mr-2">{name}</p>
+            <FaCaretDown className="text-xl " />
+          </div>
+        ) : (
+          <CiUser
+            className="text-xl cursor-pointer"
+            onClick={() => navigate('/signup')}
+          />
+        )}
+        {activeMenu && name && (
+          <div className="absolute w-60 top-7">
+            <ul>
+              <li className="py-2 px-4 bg-gray-100 cursor-pointer hover:bg-gray-800 hover:text-white">
+                {name}
+              </li>
+              <li
+                onClick={() => handleLogout()}
+                className="py-2 px-4 bg-gray-100 cursor-pointer hover:bg-gray-800 hover:text-white"
+              >
+                Logout
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   )
